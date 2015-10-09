6c7e3a88c90d0eb4e23cf210a2efb9b92f7b7859
Vegard Solheim Theriault <vegather@icloud.com>
2015-10-09 18:02:50 +0200

    Initial Commit

---
+++ Pathfinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pathfinder
+//
+//  Created by Vegard Solheim Theriault on 09/10/15.
+//  Copyright © 2015 Vegard Solheim Theriault. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
